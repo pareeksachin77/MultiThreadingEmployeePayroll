50d48ea0f1a284e16475f9b3f3390de5ad565a98
pareeksachin77 <pareeksachin77@gmail.com>
2022-12-21 20:13:52 +0530

    MultiThreadingEmployeePayroll

---
+++ MultiThreadingEmployeePayroll/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadEmpPayroll
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome To Multi Threading Problem");
+        }
+    }
+}
+/*
+Unit3TestExecutor discovered 1 of 1 NUnit test cases using Current Discovery mode, Non-Explicit run
+Employee Being Added :Eren
+Employee added : Eren
+Employee Being Added :Levi
+Employee added : Levi
+Employee Being Added :Mikasa
+Employee added : Mikasa
+Employee Being Added :Naruto
+Employee added : Naruto
+Employee Being Added :Sasuke
+Employee added : Sasuke
+Employee Being Added :Madara
+Employee added : Madara
+Employee Being Added :Luffy
+Employee added : Luffy
+Employee Being Added :Zorro
+Employee added : Zorro
+Employee Being Added :Nami
+Employee added : Nami
+Employee Being Added :Naruto
+Employee added : Naruto
+System.Collections.Generic.List`1[MultithreadEmpPayroll.EmpData]
+Duration Without thread: 00:00:00.0006554
+ */
